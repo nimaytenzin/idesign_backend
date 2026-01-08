25298b3bece961aca92b1cfc3f844513a247d1dc
nimaytenzin <nimaytenzin@gmail.com>
2026-01-08 08:32:21 +0600

    ok

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('')
 export class AppController {
-  constructor() {}
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getWelcome() {
+    return this.appService.getWelcome();
+  }
 }
