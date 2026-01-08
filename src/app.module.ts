6a5887fb3ea1e6ffe59a0e270c74f3d395be1600
nimaytenzin <nimaytenzin@gmail.com>
2026-01-08 08:23:44 +0600

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductCategoryModule } from './modules/product-category/product-category.module';
 import { ProductSubCategoryModule } from './modules/product-sub-category/product-sub-category.module';
@@ -29,17 +29,7 @@ import { AffiliateModule } from './modules/affiliate/affiliate.module';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'mysql',
-      host: process.env.DB_HOST_DEVELOPMENT || 'localhost',
-      port: parseInt(process.env.DB_PORT_DEVELOPMENT) || 3306,
-      username: process.env.DB_USER_DEVELOPMENT || 'root',
-      password: process.env.DB_PASS_DEVELOPMENT || '',
-      database: process.env.DB_NAME_DEVELOPMENT || 'idesign',
-      autoLoadModels: true,
-      synchronize: true,
-      logging: false,
-    }),
+    DatabaseModule,
     AuthModule,
     ProductCategoryModule,
     ProductSubCategoryModule,
